5e71d94be8630285b99768f9bacb7e0d950225ab
Atichka <atichka@MacBook-Air-Ekaterina.local>
2020-09-01 15:38:12 +0300

    add project with component

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+    return <header className='header'>
+        <img src='https://cdn.icon-icons.com/icons2/1446/PNG/512/22218foxface_98828.png'/>
+    </header>
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Profile = () => {
+    return <div className='content'>
+        <div>
+        <img src='https://img5.goodfon.ru/wallpaper/nbig/2/f9/lisa-zima-sneg-8.jpg'/>
+        </div>
+        <div>
+            ava + description
+        </div>
+        <div>
+            My post
+        <div>
+            New post
+        </div>
+        <div>
+            <div>
+                post 1
+            </div>
+            <div>
+                post 2
+            </div>
+        </div>
+        </div>
+    </div>
+}
+
+export default Profile;
