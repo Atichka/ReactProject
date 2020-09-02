b64d33990aacb22dc072981b47cfcb848b939d50
Atichka <atichka@MacBook-Air-Ekaterina.local>
2020-09-03 00:24:53 +0300

    css-module

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import s from './Header.module.css';
 
 const Header = () => {
-    return <header className='header'>
+    return <header className={s.header}>
         <img src='https://cdn.icon-icons.com/icons2/1446/PNG/512/22218foxface_98828.png'/>
     </header>
 }
