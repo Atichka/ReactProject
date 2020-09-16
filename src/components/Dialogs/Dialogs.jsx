fc34711f5482857d4bcbb7af28bfc06537511e94
Atichka <atichka@MacBook-Air-Ekaterina.local>
2020-09-16 23:38:37 +0300

    add map

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -16,7 +16,7 @@ return <div className={s.message}>{props.message}</div>
 
 const Dialogs = (props) => {
 
-  let dialogsData = [
+  let dialogs = [
     {id: 1, name: 'Dimych'},
     {id: 2, name: 'Andrey'},
     {id: 3, name: 'Sveta'},
@@ -25,7 +25,9 @@ const Dialogs = (props) => {
     {id: 6, name: 'Valera'}
   ]
 
-  let messagesData = [
+  let dialogsElement = dialogs.map( d => <DialogItem name={d.name} id={d.id}/> );
+
+  let messages = [
     {id: 1, message: 'Hi'},
     {id: 2, message: 'How are your?'},
     {id: 3, message: 'Yo'},
@@ -33,15 +35,15 @@ const Dialogs = (props) => {
     {id: 5, message: 'Yo'}
   ]
 
+  let messagesElement = messages.map( m => <Message message={m.message}/> );
+
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-              <DialogItem name={dialogsData[0].name} id={dialogsData[0].id}/>
-              <DialogItem name={dialogsData[1].name} id={dialogsData[1].id}/>
+              { dialogsElement }
             </div>
             <div className={s.messages}>
-              <Message message={messagesData[0].message}/>
-              <Message message={messagesData[1].message}/>
+              { messagesElement }
             </div>
         </div>
     )
